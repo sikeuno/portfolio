8b49310f64d8748a062b282839f04844cfd35866
Waris Ibrahim <95417817+sikeuno@users.noreply.github.com>
2023-05-09 15:11:07 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,7 +4,7 @@ import {MdArrowForwardIos}from "react-icons/md"
 import {Link} from "react-scroll"
 const Home = () => {
   return (
-    <div name="home" className='h-screen w-full pt-20 pb-20 bg-black'>
+    <div name="home" className='h-screen w-full pt-28 pb-20 bg-black'>
         <div className=' max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row '>
             <div className=' flex flex-col justify-center h-full'>
                 <h2 className=' text-4xl font-bold sm:text-7xl text-white'>I'm  Full Stack Developer</h2>
