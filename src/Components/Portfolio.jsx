064e3ba03fbbe78163741125e2ec23e9b2ded0b4
Waris Ibrahim <95417817+sikeuno@users.noreply.github.com>
2023-05-10 18:29:53 +0530

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -30,32 +30,26 @@ const Portfolio = () => {
   ];
 
   return (
-    <div
-      name="portfolio"
-      className="bg-gradient-to-b from-black to-black w-full h-full text-white pb-16 md:h-screen"
-      style={{ height: '100%' }}
-    >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+    <div name="portfolio" className="bg-gradient-to-b from-black to-black w-full min-h-screen text-white pb-16">
+      <div className="max-w-screen-lg p-4 mx-auto">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            Porfolio
-          </p>
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
           <p className="py-6">Check out some of my work right here</p>
         </div>
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-4 sm:px-0">
           {images.map(({ id, src }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
                 alt=""
                 className="rounded-md duration-200 hover:scale-105 object-cover"
-              ></img>
+              />
 
-              <div className="flex justify-center ite">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+              <div className="flex justify-center">
+                <button className="w-full px-4 py-2 m-2 duration-200 hover:scale-105">
                   Demo
                 </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <button className="w-full px-4 py-2 m-2 duration-200 hover:scale-105">
                   Code
                 </button>
               </div>
