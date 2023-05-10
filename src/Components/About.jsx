c189611056bfa5c58034221c8ae1a3cbe359a9f5
Waris Ibrahim <95417817+sikeuno@users.noreply.github.com>
2023-05-10 18:29:11 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div name="about" className=' w-full h-screen bg-black text-white'>
-        <div className=' max-w-screen-lg p-4 mx-auto flex flex-col jusdtify-center w-full h-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 borgray50'>About</p>
-            </div>
-            <p className='text-xl mt-10'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis qui, alias quidem reiciendis libero iusto laudantium, vitae, unde quisquam sapiente fuga minima adipisci animi explicabo laboriosam autem quam? Provident assumenda totam dolor in mollitia natus magni odit! Voluptatum eum nam eveniet porro ea odit, enim odio iste nisi, incidunt labore.
-            </p>
-            <br/>
-            <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi distinctio provident at, saepe veritatis fugit sit praesentium natus veniam in iste doloremque est reprehenderit neque architecto rem consequatur, nemo atque asperiores dolores culpa animi. Voluptatibus eius dicta veritatis asperiores maiores placeat? Vitae temporibus tempore eius, voluptatum doloremque tenetur cumque quasi.</p>
+    <div name="about" className="w-full min-h-screen bg-black text-white pb-18">
+      <div className="max-w-screen-lg p-4 mx-auto">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
         </div>
+        <div className="text-xl mt-10">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis qui, alias quidem reiciendis libero iusto
+            laudantium, vitae, unde quisquam sapiente fuga minima adipisci animi explicabo laboriosam autem quam? Provident
+            assumenda totam dolor in mollitia natus magni odit! Voluptatum eum nam eveniet porro ea odit, enim odio iste
+            nisi, incidunt labore.
+          </p>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi distinctio provident at, saepe veritatis fugit
+            sit praesentium natus veniam in iste doloremque est reprehenderit neque architecto rem consequatur, nemo atque
+            asperiores dolores culpa animi. Voluptatibus eius dicta veritatis asperiores maiores placeat? Vitae temporibus
+            tempore eius, voluptatum doloremque tenetur cumque quasi.
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
